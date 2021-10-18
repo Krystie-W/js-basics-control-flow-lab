ae242bb340dd20e896a9d6cdd07f2c12f876902e
Krystie-W <tsukio.scoring@gmail.com>
2021-10-19 10:48:39 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+    if (number <= 400) {
+      return "This one is on me!";
+    } else if (number > 2500) {
+      return "No can do.";
+    } else if (number > 2000) {
+      return "I will gladly take your thirty bucks.";
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//console.log(scuberGreetingForFeet(2501));
+
+function ternaryCheckCity(city){
+  const message = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//console.log(ternaryCheckCity('NYC'));
+
+function switchOnCharmFromTip(tipType){
+  switch (tipType) {
+    case 'generous':
+      return "Thank you so much.";
+        break;
+    case 'not as generous':
+      return "Thank you.";
+        break;
+    default:
+        return "Bye.";
+        break;
+    }
 }
+console.log(switchOnCharmFromTip("I'm not paying you"));
